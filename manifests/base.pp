23a25f730ccb33d5d10abdfbf50e7b5e17b16a6f
root <root@vamsimaster.puppet.com>
2017-10-12 12:12:04 +0530

    adding profile for using ntp module

---
+++ manifests/base.pp
@@ -0,0 +1,19 @@
+class profile::base(
+
+  $ntp_servers = [
+    '0.ubuntu.pool.ntp.org',
+    '1.ubuntu.pool.ntp.org',
+  ],
+) {
+
+	class { '::ntp':
+      	  servers => $ntp_servers,
+    }
+
+    if $facts['os']['family'] == 'RedHat' {
+      include ::profile::selinux
+    }
+
+
+
+}
